88a9275e6e520713f2e2b9c8ba0a4fc7b76682e7
fawwad123 <md101a@MD101As-MacBook-Pro-2.local>
2021-02-20 20:46:18 +0500

    update constant

--- calculator/lib/util/constant.dart
+++ calculator/lib/util/constant.dart
@@ -4,16 +4,12 @@ import 'package:flutter/services.dart';
 
 const SPLASH_DELAY = 5;
 const GREEN_COLOR = Color.fromRGBO(46, 184, 46, 1);
-const ROYAL_BLUE_COLOR = Color.fromRGBO(11, 13, 52, 1);
-const FACEBOOK_COLOR = Color.fromRGBO(59, 89, 152, 1);
-const GOOGLE_COLOR = Color.fromRGBO(219, 50, 54, 1);
 const LIGHT_BLUE_COLOR = Color.fromRGBO(191, 233, 245, 1);
 const LIGHT_GREEN_COLOR = Color.fromRGBO(190, 236, 197, 1);
 const LIGHT_PINK_COLOR = Color.fromRGBO(255, 228, 217, 1);
 const PINK_COLOR = Color.fromRGBO(255, 221, 220, 1);
 const LIGHT_GREY_COLOR = Color.fromRGBO(244, 240, 239, 1);
 const LIGHT_GREY_BORDER_COLOR = Color.fromRGBO(244, 240, 239, 1);
-const GREEN_SHADE = Color.fromRGBO(234, 248, 248, 1);
 const WHITE_COLOR = Colors.white;
 const GREY_COLOR = Colors.grey;
 const DARK_GREY_COLOR = Color.fromRGBO(128, 128, 128, 1);
